09df2f97b4bb9df96c9b52d25b53c75d26f7aad2
Saif Quazi <147558987+Saif-Quazi@users.noreply.github.com>
2025-01-21 19:00:42 +0000

    Fixed Pose Detection

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/CMBhs4EAW/";
+let URL = "https://teachablemachine.withgoogle.com/models/vztbEYP23/";
 
 let model, webcam, ctx, labelContainer, maxPredictions;
 
@@ -228,7 +228,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/CMBhs4EAW/");
+        console.log("https://teachablemachine.withgoogle.com/models/vztbEYP23/");
     }
 }
 
